9acb818dde3dd2dcfa1b30e8cad6f4e89e4be481
HOME <HOME@DESKTOP-VUVTT19>
2025-07-20 01:27:36 +0530

    This is Empty Controller

---
+++ WebApi.DotNetV8/Controllers/EmptyController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.DotNetV8.Controllers
+{
+    public class EmptyController : Controller
+    {
+        // I have added this controller to KnowledgeBase.
+    }
+}
