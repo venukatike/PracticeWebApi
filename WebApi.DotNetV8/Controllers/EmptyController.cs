55e74e84cfa35c77cac91747d77aa3cb90308438
HOME <HOME@DESKTOP-VUVTT19>
2025-07-20 01:34:00 +0530

    This

--- WebApi.DotNetV8/Controllers/EmptyController.cs
+++ WebApi.DotNetV8/Controllers/EmptyController.cs
@@ -5,5 +5,10 @@ namespace WebApi.DotNetV8.Controllers
     public class EmptyController : Controller
     {
         // I have added this controller to KnowledgeBase.
+
+        public EmptyController()
+        {
+            //Added constructor to demonstrate a simple controller without any actions.
+        }
     }
 }
