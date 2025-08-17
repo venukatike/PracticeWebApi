3aeadf1ab2de1e19a3130206bfbd9e5fd1378b1b
HOME <HOME@DESKTOP-VUVTT19>
2025-08-17 18:19:47 +0530

    #migrations

---
+++ CodeFirst-V8/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CodeFirst_V8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst_V8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Patient> patients { get; set; }
+        DbSet<CPT> refCPT { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Corrected the Ignore method usage to ignore the Patient entity type  
+            modelBuilder.Ignore<Patient>();
+        }
+    }
+}
---
+++ CodeFirst-V8/Models/Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeFirst_V8.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
