a42ce4fa728d4d58201114c5f7058f9b7f81521d
venukatike <venukatike02@gmail.com>
2025-09-29 00:06:35 +0530

    ArraySecondLargestElement.SecondLargestElement(arr);

--- CSharpCoding/Program.cs
+++ CSharpCoding/Program.cs
@@ -1,39 +1,16 @@
-﻿// Write a String reverse without using any bulit- in methods
-using System;
+﻿
+// Find the second largest element in an array 
 
+using System;
+using CSharpCoding;
 
 class Program
 {
     public static void Main(string[] args)
     {
-		Console.WriteLine("Enter the Number : ");
-	int number = int.Parse(Console.ReadLine());
-		Program program = new Program();
-		Console.WriteLine(program.sumOfNumbers(number));
-	}
-
-	int t1, results = 0;
-    public int sumOfNumbers(int number)
-	{
-		while(number !=0){
-		t1 = number % 10;
-		number = number / 10;
-        results = results + t1;
-}
-		return results;
-	}
-
+        int[] arr = [10, 20, 60, 40, 50];
+        ArraySecondLargestElement.SecondLargestElement(arr);
+    }   
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+ 
